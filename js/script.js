ceda027c98f0695e4b4074c9a93cda61e745d05d
Mariusz Jarosz <m.jarosz82@gmail.com>
2021-05-18 08:56:14 +0200

    added js script

---
+++ js/script.js
@@ -0,0 +1,72 @@
+{
+    const tasks = [];
+
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    }
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    }
+
+    const toggleDoneTask = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const bindEvents = () => {
+        const removeButtons = document.querySelectorAll(".js-remove");
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            })
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+        toggleDoneButtons.forEach((toggleDoneButton, taskIndex) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleDoneTask(taskIndex);
+            })
+        })
+    }
+
+    const render = () => {
+        let htmlString = "";
+        for (const task of tasks) {
+            htmlString += `
+                <li${task.done ? " class = \"list__taskItem--done\"" : ""}>
+                <button class = "js-done">Zrobione</button>
+                ${task.content}
+                <button class = "js-remove">Usuń</button>
+                </li>
+            `;
+        }
+        document.querySelector(".js-taskList").innerHTML = htmlString;
+        bindEvents();        
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        const newTaskContent = document.querySelector(".js-inputTask").value.trim();
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
+    }
+
+    const init = () => {
+
+        const form = document.querySelector(".js-form");
+        form.addEventListener("submit", () => {
+            onFormSubmit(event);
+            form.reset();
+        });
+
+    }
+
+    init();
+}
